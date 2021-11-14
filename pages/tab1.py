4f05a7e32c6e2ddbc61d18a66c9f13a714025bef
Rogers D. Stephens <rstephens41@gatech.edu>
2021-11-14 15:17:11 -0600

    fix typo

--- pages/tab1.py
+++ pages/tab1.py
@@ -3,7 +3,7 @@ import hydralit as st
 def run():
     html_temp = """ 
     <div style ="background-color:black;padding:1px"> 
-    <h3 style ="color:white;text-align:center;">Questions and viz of feature importannce</h3> 
+    <h3 style ="color:white;text-align:center;">Questions and viz of feature importance</h3> 
     </div> 
     """
     st.markdown(html_temp, unsafe_allow_html = True)
