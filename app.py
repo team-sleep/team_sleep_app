2e4ed9c57b1785673fae7373070f1125d5507055
erichard7 <93166113+erichard7@users.noreply.github.com>
2021-11-14 22:20:20 -0500

    Update app.py
    Fix indents

--- app.py
+++ app.py
@@ -42,59 +42,59 @@ def prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History):
         return pred
 
 def find_importances (Gender, Married, ApplicantIncome, LoanAmount, Credit_History):   
-    if Gender == "Male":
-            Gender = 0
-    else:
-        Gender = 1
+	if Gender == "Male":
+		Gender = 0
+    	else:
+        	Gender = 1
 
-    if Married == "Unmarried":
-        Married = 0
-    else:
-        Married = 1
+    	if Married == "Unmarried":
+       		Married = 0
+    	else:
+        	Married = 1
 
-    if Credit_History == "Unclear Debts":
-        Credit_History = 0
-    else:
-        Credit_History = 1  
+    	if Credit_History == "Unclear Debts":
+		Credit_History = 0
+   	else:
+		Credit_History = 1  
 
-    LoanAmount = LoanAmount / 1000
+    	LoanAmount = LoanAmount / 1000
 
-    d = {'Gender': [Gender], 'Married': [Married], "ApplicantIncome":[ApplicantIncome], "LoanAmount":[LoanAmount], "Credit_History":[Credit_History] }
-    df = pd.DataFrame(data=d)
-
-
-    from treeinterpreter import treeinterpreter as ti
-    prediction, bias, contributions = ti.predict(classifier, df)
-    N = 6 # no of entries in plot , 4 ---> features & 1 ---- class label
+    	d = {'Gender': [Gender], 'Married': [Married], "ApplicantIncome":[ApplicantIncome], "LoanAmount":[LoanAmount], "Credit_History":[Credit_History] }
+    	df = pd.DataFrame(data=d)
 
 
-    import matplotlib.pyplot as plt
-    import numpy as np
+    	from treeinterpreter import treeinterpreter as ti
+    	prediction, bias, contributions = ti.predict(classifier, df)
+    	N = 6 # no of entries in plot , 4 ---> features & 1 ---- class label
 
-    col = ['Gender', 'Married', 'ApplicantIncome', 'LoanAmount', 'Credit_History',"result"]
 
-    one = []
-    zero = []
+    	import matplotlib.pyplot as plt
+    	import numpy as np
 
-    for j in range(2):
-        list_ =  [one ,zero]
-        for i in range(5):
-            val = contributions[0,i,j]
-            list_[j].append(val)
+    	col = ['Gender', 'Married', 'ApplicantIncome', 'LoanAmount', 'Credit_History',"result"]
 
-    zero.append(prediction[0,0]/6)
-    one.append(prediction[0,1]/6)
-    fig, ax = plt.subplots()
-    ind = np.arange(N)   
-    width = 0.15        
-    p1 = ax.bar(ind, one, width, color='red', bottom=0)
-    p2 = ax.bar(ind+width, zero, width, color='green', bottom=0)
-    ax.set_title('Contribution of all feature for a particular sample')
-    ax.set_xticks(ind + width / 2)
-    ax.set_xticklabels(col, rotation = 90)
-    ax.legend((p1[0], p2[0]), ('one', 'zero') , bbox_to_anchor=(1.04,1), loc="upper left")
-    ax.autoscale_view()
-    st.pyplot(fig)
+    	one = []
+    	zero = []
+
+    	for j in range(2):
+        	list_ =  [one ,zero]
+        	for i in range(5):
+            	val = contributions[0,i,j]
+            	list_[j].append(val)
+
+    	zero.append(prediction[0,0]/6)
+    	one.append(prediction[0,1]/6)
+    	fig, ax = plt.subplots()
+    	ind = np.arange(N)   
+    	width = 0.15        
+    	p1 = ax.bar(ind, one, width, color='red', bottom=0)
+    	p2 = ax.bar(ind+width, zero, width, color='green', bottom=0)
+    	ax.set_title('Contribution of all feature for a particular sample')
+    	ax.set_xticks(ind + width / 2)
+    	ax.set_xticklabels(col, rotation = 90)
+    	ax.legend((p1[0], p2[0]), ('one', 'zero') , bbox_to_anchor=(1.04,1), loc="upper left")
+    	ax.autoscale_view()
+    	st.pyplot(fig)
     
 
 
@@ -105,28 +105,28 @@ app = st.HydraApp(title='Team Sleep App', nav_horizontal=True,
 @app.addapp(title="User", is_home=True) #set to false to not use "home icon" for this page
 def tab1():
 	from pages import tab1
-    st.sidebar.title("Sleep Questionnaire")
-    Gender = st.sidebar.selectbox('Gender',("Male","Female"))
-    Married = st.sidebar.selectbox('Marital Status',("Unmarried","Married")) 
-    ApplicantIncome = st.sidebar.number_input("Applicants monthly income") 
-    LoanAmount = st.sidebar.number_input("Total loan amount")
-    Credit_History = st.sidebar.selectbox('Credit_History',("Unclear Debts","No Unclear Debts"))
-    result =""
+    	st.sidebar.title("Sleep Questionnaire")
+    	Gender = st.sidebar.selectbox('Gender',("Male","Female"))
+    	Married = st.sidebar.selectbox('Marital Status',("Unmarried","Married")) 
+    	ApplicantIncome = st.sidebar.number_input("Applicants monthly income") 
+    	LoanAmount = st.sidebar.number_input("Total loan amount")
+    	Credit_History = st.sidebar.selectbox('Credit_History',("Unclear Debts","No Unclear Debts"))
+    	result =""
     
-    if st.sidebar.button("Predict"): 
-        result = prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History) 
-        st.sidebar.success('Your loan is {}'.format(result))
-        print(LoanAmount)
+    	if st.sidebar.button("Predict"): 
+        	result = prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History) 
+        	st.sidebar.success('Your loan is {}'.format(result))
+        	print(LoanAmount)
         
         
-       # feat_importances = pd.Series(importances.feature_importances_, index=["Gender", "Married", "ApplicantIncome", "LoanAmount", "Credit_History"]).sort_values(ascending=False)
-       # impPlot(feat_importances, 'Random Forest Classifier')
-        find_importances(Gender, Married, ApplicantIncome, LoanAmount, Credit_History)  
-        st.write('\n')
+       	# feat_importances = pd.Series(importances.feature_importances_, index=["Gender", "Married", "ApplicantIncome", "LoanAmount", "Credit_History"]).sort_values(ascending=False)
+       	# impPlot(feat_importances, 'Random Forest Classifier')
+        	find_importances(Gender, Married, ApplicantIncome, LoanAmount, Credit_History)  
+        	st.write('\n')
    
 
-  #  @st.cache()
-    tab1.run()
+ 	 #  @st.cache()
+    	tab1.run()
 
 @app.addapp(title="Model")
 def tab2():
