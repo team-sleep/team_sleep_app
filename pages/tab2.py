a9b4c2b80b90b7700d229c540305503319ff2a91
Brenna Radtke <brennamollet@Brennas-MacBook-Air.local>
2021-11-15 17:11:30 -0600

    Add feature importance bar chart

--- pages/tab2.py
+++ pages/tab2.py
@@ -1,4 +1,6 @@
 import hydralit as st
+import pickle
+from matplotlib import pyplot as plt
 
 def run():
     html_temp = """ 
@@ -7,4 +9,13 @@ def run():
     </div> 
     """
     st.markdown(html_temp, unsafe_allow_html = True)
-    st.sidebar.title("Model Visualization")
+    st.title("Model Visualization")
+
+    pickle_in = open('classifier.pkl', 'rb') 
+    classifier = pickle.load(pickle_in)
+    print(type(classifier))
+    feature_importance = classifier.feature_importance_
+    print(feature_importance)
+    feature_names = ["Gender", "Married", "ApplicantIncome", "LoanAmount", "Credit_History"]
+    st.bar_chart(feature_importance, columns = feature_names)
+
