acbb2ba6b5e2a5e3ad86883f4d29d8e7360be3eb
Brenna Radtke <brennamollet@Brennas-MacBook-Air.local>
2021-11-22 20:24:22 -0600

    add confusion matrix

--- pages/tab2.py
+++ pages/tab2.py
@@ -2,6 +2,8 @@ import hydralit as st
 import pickle
 import altair as alt
 import pandas as pd
+import plotly.figure_factory as ff
+
 
 def run():
     html_temp = """ 
@@ -28,5 +30,44 @@ def run():
     st.altair_chart(c, use_container_width=True)
     
     st.subheader("Confusion Matrix") 
-    st.write("Confusion Matrix Visual will be placed here.")
+    
+    z = [[73, 44],
+     [20, 116]]
+
+    x = ['Non-Apnea', 'Sleep Related Breating Disorder Apnea']
+    y = ['Non-Apnea', 'Sleep Related Breating Disorder Apnea']
+
+    # change each element of z to type string for annotations
+    z_text = [[str(y) for y in x] for x in z]
+
+    # set up figure 
+    fig = ff.create_annotated_heatmap(z, x=x, y=y, annotation_text=z_text, colorscale='Viridis')
+
+
+    # add custom xaxis title
+    fig.add_annotation(dict(font=dict(color="black",size=14),
+                            x=0.5,
+                            y=-0.15,
+                            showarrow=False,
+                            text="Predicted value",
+                            xref="paper",
+                            yref="paper"))
+
+    # add custom yaxis title
+    fig.add_annotation(dict(font=dict(color="black",size=14),
+                            x=-0.35,
+                            y=0.5,
+                            showarrow=False,
+                            text="Real value",
+                            textangle=-90,
+                            xref="paper",      
+                            yref="paper"))
+
+    # adjust margins to make room for yaxis title
+    fig.update_layout(margin=dict(t=50, l=200))
+
+    # add colorbar
+    fig['data'][0]['showscale'] = True
+    # fig.show()
+    st.plotly_chart(fig, use_container_width=True)
 
