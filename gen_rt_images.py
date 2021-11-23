eece14370cef83999588f053cc1534ba710f85e1
Rogers D. Stephens <rstephens41@gatech.edu>
2021-11-22 21:09:13 -0600

    switch rt viz to use actual model

--- gen_rt_images.py
+++ gen_rt_images.py
@@ -1,55 +1,23 @@
 import pickle
 from sklearn.tree import export_graphviz
 
-
-def gen_cols(max_columns_on_page=5):
-# loading the trained model
-	pickle_in = open('loan_classifier.pkl', 'rb') 
-	classifier = pickle.load(pickle_in)
-
-	print("total random trees =",len(classifier.estimators_))
-	print("writing images supporting a layout with", max_columns_on_page, "columns")
-
-	row=-1
-
-	for i in range(len(classifier.estimators_)):
-		col = i % max_columns_on_page
-		
-		if(col==0):
-			row=row + 1
-
-		outfile="images/rtree_row" + str(row) + "_col" + str(col)
-		estimator = classifier.estimators_[i]
-
-		export_graphviz(estimator, out_file=outfile + ".dot", 
-						feature_names = ['Gender', 'Married', 'ApplicantIncome', 'LoanAmount', 'Credit_History'],
-						class_names = ['Not_Approved', "Approved"],
-						rounded = True, proportion = False, 
-						precision = 2, filled = True)
-
-		# Convert to png using system command (requires Graphviz)
-		from subprocess import call
-		call(['dot', '-Tpng', outfile + '.dot', '-o', outfile + '.png', '-Gdpi=200'])
-	
-	print("generated images for a grid with ", row+1, "rows and ", col + 1, "columns")
-	pass
-
 def gen_with_seq_number():
-# loading the trained model
-	pickle_in = open('loan_classifier.pkl', 'rb') 
+	# loading the trained model
+	pickle_in = open('classifier_rf.pkl', 'rb') 
 	classifier = pickle.load(pickle_in)
 
 	print("total random trees =",len(classifier.estimators_))
 	print("appending number on each rt image from 0 to", str(len(classifier.estimators_) - 1))
 
 	for i in range(len(classifier.estimators_)):
-		outfile="images/rtree_image_" + str(i)
+		outfile = "images/rtree_image_" + str(i)
 		estimator = classifier.estimators_[i]
+		
 		print("writing image", outfile)
-
+		
 		export_graphviz(estimator, out_file=outfile + ".dot", 
-						feature_names = ['Gender', 'Married', 'ApplicantIncome', 'LoanAmount', 'Credit_History'],
-						class_names = ['Not_Approved', "Approved"],
+						feature_names = ["What Treatment Since Enrollment Study Cpap", "MAP Score","rMEQ Total Score","What Treatment Since Enrollment Study Med","Body Mass Index (BMI)"],
+						class_names = ['Sleep_Apnea', "No_Sleep_Apnea"],
 						rounded = True, proportion = False, 
 						precision = 2, filled = True)
 
@@ -62,4 +30,4 @@ def gen_with_seq_number():
 
 if __name__=='__main__':
 	gen_with_seq_number()
-	gen_cols(max_columns_on_page=5)
+	#gen_cols(max_columns_on_page=5)
--- pages/tab3.py
+++ pages/tab3.py
@@ -3,39 +3,6 @@ import pickle
 from sklearn.tree import export_graphviz
 
 def run():
-    # html_temp = """ 
-    # <div style ="background-color:black;padding:1px"> 
-    # <h3 style ="color:white;text-align:center;">Add to tab 2 in a subsequent iteration...random forest visualization</h3> 
-    # </div> 
-    # """
-    # st.markdown(html_temp, unsafe_allow_html = True)
-
-    # load the trained model
-    # pickle_in = open('classifier.pkl', 'rb') 
-    # classifier = pickle.load(pickle_in)
-
-    # estimator = classifier.estimators_[1]
-
-    # tree = export_graphviz(estimator, 
-    #             feature_names = ['Gender', 'Married', 'ApplicantIncome', 'LoanAmount', 'Credit_History'],
-    #             class_names = ['Not_Approved', "Approved"],
-    #             rounded = True, proportion = False, 
-    #             precision = 2, filled = True)
-
-    # st.graphviz_chart(tree)
-
-    # export_graphviz(estimator, out_file='tree.dot', 
-    #                 feature_names = ['Gender', 'Married', 'ApplicantIncome', 'LoanAmount', 'Credit_History'],
-    #                 class_names = ['Not_Approved', "Approved"],
-    #                 rounded = True, proportion = False, 
-    #                 precision = 2, filled = True)
-
-    # # Convert to png using system command (requires Graphviz)
-    # from subprocess import call
-    # call(['dot', '-Tpng', 'tree.dot', '-o', 'tree.png', '-Gdpi=600'])
-
-    #st.sidebar.title("Preferences")
-    #display_num_trees = st.sidebar.select_slider("Trees to display", options=[1,4,50,100], on_change=None, args=None, kwargs=None)
     st.markdown('The random forest algorithm determines an outcome based on the predictions from **100** decision trees. ' +
             'Use the slider to visulize a subset or all of the trees in the forest.')
 
